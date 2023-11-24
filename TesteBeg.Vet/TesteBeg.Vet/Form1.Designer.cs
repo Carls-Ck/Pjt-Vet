5e788b1a5aa7847c47cfbb1442d1e99a29c7362f
Carls-Ck <carlinhos.ehg@gmail.com>
2023-11-24 15:01:06 -0300

    agora vai

---
+++ TesteBeg.Vet/TesteBeg.Vet/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TesteBeg.Vet
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.dtgConsultas = new System.Windows.Forms.DataGridView();
+            this.Médico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnConsultas = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnMedicos = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnPacientes = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsultas)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtgConsultas
+            // 
+            this.dtgConsultas.AllowUserToAddRows = false;
+            this.dtgConsultas.AllowUserToDeleteRows = false;
+            this.dtgConsultas.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dtgConsultas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgConsultas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Médico});
+            this.dtgConsultas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dtgConsultas.GridColor = System.Drawing.SystemColors.MenuHighlight;
+            this.dtgConsultas.Location = new System.Drawing.Point(20, 258);
+            this.dtgConsultas.Name = "dtgConsultas";
+            this.dtgConsultas.ReadOnly = true;
+            this.dtgConsultas.Size = new System.Drawing.Size(835, 181);
+            this.dtgConsultas.TabIndex = 0;
+            // 
+            // Médico
+            // 
+            this.Médico.HeaderText = "Column1";
+            this.Médico.Name = "Médico";
+            this.Médico.ReadOnly = true;
+            this.Médico.Width = 30;
+            // 
+            // btnConsultas
+            // 
+            this.btnConsultas.BackgroundImage = global::TesteBeg.Vet.Properties.Resources.download__1_;
+            this.btnConsultas.Font = new System.Drawing.Font("MV Boli", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultas.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnConsultas.Location = new System.Drawing.Point(310, 28);
+            this.btnConsultas.Name = "btnConsultas";
+            this.btnConsultas.Size = new System.Drawing.Size(292, 139);
+            this.btnConsultas.TabIndex = 2;
+            this.btnConsultas.Text = "Fazer Consulta";
+            this.btnConsultas.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnMedicos,
+            this.toolStripSeparator1,
+            this.btnPacientes,
+            this.toolStripSeparator2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(867, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnMedicos
+            // 
+            this.btnMedicos.Image = ((System.Drawing.Image)(resources.GetObject("btnMedicos.Image")));
+            this.btnMedicos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnMedicos.Name = "btnMedicos";
+            this.btnMedicos.Size = new System.Drawing.Size(72, 22);
+            this.btnMedicos.Text = "Médicos";
+            this.btnMedicos.Click += new System.EventHandler(this.btnMedicos_Click_1);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnPacientes
+            // 
+            this.btnPacientes.Image = ((System.Drawing.Image)(resources.GetObject("btnPacientes.Image")));
+            this.btnPacientes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPacientes.Name = "btnPacientes";
+            this.btnPacientes.Size = new System.Drawing.Size(77, 22);
+            this.btnPacientes.Text = "Pacientes";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TesteBeg.Vet.Properties.Resources._5447153_veterinario_clinica_medico_examinando_vacinacao_e_saude_para_animais_de_animais_como_caes_e_gatos_em_plano_de_fundo_ilustracao_para_cartaz_ou_banner_vetor1;
+            this.ClientSize = new System.Drawing.Size(867, 451);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnConsultas);
+            this.Controls.Add(this.dtgConsultas);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "frmPrincipal";
+            this.ShowIcon = false;
+            this.Text = "Sistema de Controle veterinario";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsultas)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgConsultas;
+        private System.Windows.Forms.Button btnConsultas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Médico;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnMedicos;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnPacientes;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ TesteBeg.Vet/TesteBeg.Vet/Medicos.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteBeg.Vet
+{
+    internal class Medico
+
+    {
+        public class Funcionarios
+        {
+            SqlCommand comandoSql = new SqlCommand();
+            StringBuilder sql = new StringBuilder();
+            DataTable dadosTabela = new DataTable();
+
+            //Método que irá Salvar as informações conforme os parâmetros que possui entre parênteses.
+            public void Salvar(string nome,
+                               string endereco,
+                               string experiencia,
+                               string area,
+                               string DP,
+                               string telefone,
+                               string Cpf)
+            {
+                try //Estrutura try, a qual tenta realizar o que está dentro de suas chaves.
+                {
+                    //Estabelece a conexão com o banco através da string de conexão.
+                    using (SqlConnection conexao = new SqlConnection(Conexao.stringConexao))
+                    {
+                        conexao.Open(); //Abre a conexão com o banco de dados.
+
+                        //Comando SQL para a inserção de valores nos respectivos campos da tabela Funcionário.
+                        sql.Append("INSERT INTO Medico (NOME_MEDICO, ENDERECO_MEDICO, EXPERIENCIA_MEDICO  ");
+                        sql.Append("AREA_MEDICO, DISPONIBILIDADE_MEDICO, TELEFONE_MEDICO, CPF_MEDICO, CPF_MEDICO) ");
+
+                        sql.Append(" VALUES (@nome, @endereco, @experiencia, @area, @DP, @telefone, @Cpf)");
+
+                        //Relaciona cada valor com seu respectivo parâmetro.
+                        comandoSql.Parameters.Add(new SqlParameter("@nome", nome));
+                        comandoSql.Parameters.Add(new SqlParameter("@endereco", endereco));
+                        comandoSql.Parameters.Add(new SqlParameter("@experiencia", experiencia));
+                        comandoSql.Parameters.Add(new SqlParameter("@area", area));
+                        comandoSql.Parameters.Add(new SqlParameter("@DP", DP));
+                        comandoSql.Parameters.Add(new SqlParameter("@telefone", telefone));
+                        comandoSql.Parameters.Add(new SqlParameter("@Cpf", Cpf));
+
+
+                        comandoSql.CommandText = sql.ToString(); //Indica que o código SQL é o que deverá ser executado.
+                        comandoSql.Connection = conexao; //Indica que a conexão dos comandos SQL é a string de conexão.
+                        comandoSql.ExecuteNonQuery(); //Executa todo o comando para a inserção dos valores.
+                    }
+                }
+                catch (Exception)
+                {
+                    //Caso ocorra um erro no bloco try, entrará no catch, lugar em irá detectá-lo.
+                    throw new Exception("Ocorreu um erro no método Salvar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+                }
+            }
+
+            //Método que armazena as informações da tabela Funcionário e retorna esses dados em ordem decrescente, baseado pelo id.
+            public DataTable Listar()
+            {
+                try
+                {
+                    using (SqlConnection conexao = new SqlConnection(Conexao.stringConexao))
+                    {
+                        conexao.Open();
+
+                        sql.Append("SELECT * FROM Medico");
+                        sql.Append(" ORDER BY ID_Medico DESC");
+
+                        comandoSql.CommandText = sql.ToString();
+                        comandoSql.Connection = conexao;
+                        dadosTabela.Load(comandoSql.ExecuteReader());
+                        return dadosTabela;
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Ocorreu um erro no método Listar. Caso o problema persista, entre em contato com o Administrador do sistema.");
+                }
+            }
+
+            //Método que irá Atualizar as informações conforme os parâmetros que possui entre parênteses.
+            public void Alterar(int ID_Medico, string nome, string endereco,
+                                string experiencia, string area, DateTime DP,string telefone, string Cpf)
+            {
+                try
+                {
+                    using (SqlConnection conexao = new SqlConnection(Conexao.stringConexao))
+                    {
+                        conexao.Open();
+
+                        sql.Append("UPDATE Funcionario");
+                        sql.Append(" SET NOME_MEDICO=@ID_Medico, ENDERECO_MEDICO=@endereco, EXPERIENCIA_MEDICO@experiencia, AREA_MEDICO=@area");
+                        sql.Append("DISPONIBLIDADE_MEDICO=@DP, TELEFONE_MEDICO=@telefone, CPF_MEDICO=@cpf");
+
+                        sql.Append(" WHERE (ID_Medico=@idMedico)");
+
+                        comandoSql.Parameters.Add(new SqlParameter("@nome", nome));
+                        comandoSql.Parameters.Add(new SqlParameter("@endereco", endereco));
+                        comandoSql.Parameters.Add(new SqlParameter("@experiencia", experiencia));
+                        comandoSql.Parameters.Add(new SqlParameter("@area", area));
+                        comandoSql.Parameters.Add(new SqlParameter("@DP", DP));
+                        comandoSql.Parameters.Add(new SqlParameter("@telefone", telefone));
+                        comandoSql.Parameters.Add(new SqlParameter("@cpf", Cpf));
+
+                        comandoSql.CommandText = sql.ToString();
+                        comandoSql.Connection = conexao;
+                        comandoSql.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Ocorreu um erro no método Alterar. Caso o problema persista, entre em contato com o Administrador do Sistema.");
+                }
+            }
+
+            //Método para excluir registros do banco.
+            public void Excluir(int idMedico)
+            {
+                try
+                {
+                    using (SqlConnection conexao = new SqlConnection(Conexao.stringConexao))
+                    {
+                        conexao.Open();
+
+                        sql.Append("DELETE FROM Medico");
+                        sql.Append(" WHERE (ID_MEDICO = @idMedico)");
+
+                        comandoSql.Parameters.Add(new SqlParameter("@idFuncionario", idMedico));
+
+                        comandoSql.CommandText = sql.ToString();
+                        comandoSql.Connection = conexao;
+                        comandoSql.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Ocorreu um erro no método Excluir. Caso o problema persista, entre em contato com o Administrador do sistema.");
+                }
+            }
+        }
+    }
+}
