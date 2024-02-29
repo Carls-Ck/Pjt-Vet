b8a1afe5a7d433825164d83d87f420b025664308
Carls-Ck <carlinhos.ehg@gmail.com>
2024-02-29 17:03:33 -0300

    ujust 1

---
+++ TesteBeg.Vet/TesteBeg.Vet/dbo.Consulta.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Consulta] (
+    [ID_Consults] INT      NOT NULL,
+    [Date]        DATE     NOT NULL,
+    [Time]     DATETIME NOT NULL,
+    [ID_Médico]   INT      NOT NULL,
+    [ID_Pacient] INT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID_Consults] ASC)
+);
+
